d791cba572b4e0b14c6c424b1bf6ffe175447aff
Glovindan <turbo69nice@gmail.com>
2020-11-06 20:17:12 +0400

    added Error page (a little funny)

--- src/pages/error/ErrorPage.tsx
+++ src/pages/error/ErrorPage.tsx
@@ -2,9 +2,17 @@ import React from "react";
 
 import styles from './ErrorPage.module.pcss';
 
+import Hole from '../../assets/hole.png'
+import Hole1 from '../../assets/hole1.png'
 const ErrorPage = (): React.ReactElement => {
   return (
-      <div>ErrorPage</div>
+      <div className={styles.wrapper}>
+        <div className={styles.container}>
+          <img src={Hole} />
+          404<br/>Page Not Found
+          <img src={Hole1} />
+        </div>
+      </div>
   )
 };
 
