424815228e25889298f40dc7645d486ce21b156c
Ivan Tyulkin <phantomlancerinyourmind@gmail.com>
2020-11-16 09:56:51 +0300

    [add] edit button for info component

--- src/pages/myPage/components/Info/Info.module.pcss.d.ts
+++ src/pages/myPage/components/Info/Info.module.pcss.d.ts
@@ -2,5 +2,8 @@ declare const styles: {
   readonly container: string;
   readonly subject: string;
   readonly info: string;
+  readonly editButton: string;
+  readonly pressed: string;
+  readonly editImage: string;
 };
 export = styles;
--- src/pages/myPage/components/Info/Info.tsx
+++ src/pages/myPage/components/Info/Info.tsx
@@ -1,24 +1,45 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
+import cn from "clsx"
+
+import EditImage from '../../../../assets/edit.png'
 
 import styles from './Info.module.pcss';
 
 type InfoProps = {
     subject: string;
-    info: string;
 }
 
 const Info = (props: InfoProps):React.ReactElement => {
     
-    const {subject, info} = props;
+    const {subject } = props;
+
+    const [info, setInfo] = useState<String>('[ДАННЫЕ УДАЛЕНЫ]');
+    const [editMode, setEditMode] = useState<boolean>(false);
+
+    const spanInfo = useRef<HTMLElement>(document.createElement("span"));
+
+    const handleClickActionButton = () => {
+        if (editMode) {
+            spanInfo.current.removeAttribute('contentEditable');
+            
+        } else {
+            spanInfo.current.focus();
+            spanInfo.current.setAttribute('contentEditable', "true");
+        }
+        setEditMode(!editMode);
+    };
 
     return(
         <div className={styles.container}>
             <span className={styles.subject}>
                 {subject}:
             </span>
-            <span className={styles.info}>
+            <span ref={spanInfo} className={styles.info}>
                 {info}
             </span>
+            <button className={cn(styles.editButton, editMode && styles.pressed)} onClick={()=>handleClickActionButton()} >
+                <img className={styles.editImage} src={EditImage}></img>
+            </button>
         </div>
     )
 };
