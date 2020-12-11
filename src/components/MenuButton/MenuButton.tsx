843eb1556e99267dfc2d0355ff7e1b215bc8662e
Ivan Tyulkin <phantomlancerinyourmind@gmail.com>
2020-12-11 12:43:40 +0300

    [fix] add files

---
+++ src/components/MenuButton/MenuButton.tsx
@@ -0,0 +1,21 @@
+import React from "react"
+
+import { NavLink } from "react-router-dom"
+
+type MenuButtonProps = {
+  className?: string;
+  href: string;
+  text: string;
+}
+
+const MenuButton = (props: MenuButtonProps): React.ReactElement => {
+  const {className, href, text} = props;
+
+  return (
+    <NavLink className={className} to={href}>
+      {text}
+    </NavLink>
+  );
+};
+
+export default MenuButton;
